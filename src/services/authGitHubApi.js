0b605c8fc66399e4b12ac1ca2652633f033f3bea
Weiller Carvalho <engweiller@hotmail.com>
2023-01-13 12:24:04 -0300

    feat: correcao ouath

--- src/services/authGitHubApi.js
+++ src/services/authGitHubApi.js
@@ -1,7 +1,12 @@
 import api from './api';
 
 export async function signInWithGitHubApi(email, token) {
-  const response = await api.post('/auth/sign-in/github', { email, token });
+  const response = await api.post('/auth/sign-in/github', {}, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Email: email
+    },
+  });
   console.log('🚀🚀🚀 ~ file: authGitHubApi.js:6 ~ signInWithGitHubApi ~ response', response);
   return response.data;
 }
